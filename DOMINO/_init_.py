4ece6db6e82c021d8e94abbdde1bc4a1f7c71dba
programmerjia <88567488+programmerjia@users.noreply.github.com>
2025-10-13 11:31:07 +0800

    Update _init_.py

--- DOMINO/_init_.py
+++ DOMINO/_init_.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 """
-# Author: 
+# Author: Pan Jia, Wenjun Liu, Ning Liu, and Fuyi Li
 # File Name: __init__.py
 # Description:
 """
 
-__author__ = ""
-__email__ = ""
+__author__ = "Pan Jia, Wenjun Liu, Ning Liu, and Fuyi Li"
+__email__ = "jiapan@nwafu.edu.cn; nora.liu@adelaide.edu.au; ning.liu@adelaide.edu.au; fuyi.li@nwafu.edu.cn"
 
 from .cluster import clustering
 from .preprocess import optimized_construct_interaction, preprocess, grid_downsample, get_feature, add_contrastive_label, preprocess_adj_sparse
